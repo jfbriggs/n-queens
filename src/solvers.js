3ce1329d9969e685285037b2af0e7439eaf43925
Hack Reactor Students <accounts@hackreactor.com>
2016-12-22 15:54:27 -0800

    Arrive at initial working solution for 4 rooks

--- src/solvers.js
+++ src/solvers.js
@@ -16,17 +16,21 @@
 
 
 window.findNRooksSolution = function(n) {
-  // var solution = undefined; //fixme
-  
-  /* BS SAMPLE CODE:
-  var tempSolution = new Board([[0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0], [1, 0, 0, 0]]);
+  var solution;
 
-  if (tempSolution.hasAnyRooksConflicts() === false) {
-    solution = tempSolution.rows();
-  } */
+  var createStartingArray = function(n) {  // creates a single array of n*n values with the first n values being 1
+    var result = [];
+    for (var i = 1; i <= n; i++) {
+      result.push(1);
+    }
+    for (var j = 1; j <= n * (n - 1); j++) {
+      result.push(0);
+    }
+    return result;
+  };
 
   var matricize = function(n, array) {
-    var result = [];  // final matrix outer array
+    var result = [];  // will become final matrix outer array
 
     // iterate through passed in array (n * n number of elements), increment by n
     for (var i = 0; i < array.length; i += n) {
@@ -36,73 +40,214 @@ window.findNRooksSolution = function(n) {
     
     //return newly formed matrix array result
     return result;
-
   };
 
-  // function to check combinations
+  // create starting test Board based on n (entire first row will be 1's)
+  var testBoard = new Board(matricize(n, createStartingArray(n)));
 
-  var runThroughCombos = function(n, array) {
-
-  // create Board out of current passed in combination
-    var tempBoard = new Board(matricize(n, array));
-
-    // if that Board passes Rook tests
-    if (tempBoard.hasAnyRooksConflicts() === false) {
-      // return that board's rows() output
-      return tempBoard.rows();
+  // create set of starting indices based on n
+  var positions = function() {
+    var result = [];
+    for (var i = 0; i < n; i++) {
+      result.push([0, i]);
     }
+    return result;
+  }();  // i.e. [[0, 0], [1, 3], [2, 0], [3, 3]] ==> starting indices for n = 4 ([row, column])
 
-    var theOnes = [];
-    var last = Math.sqrt(array.length) - 1;
-
-    for (var i = 0; theOnes.length < 4; i++) {
-      if (array[i] === 1) {
-        theOnes.push(i);
-      }
-    }
-
-    if (theOnes === [array.length - 1, array.length - 2, array.length - 3, array.length - 4]) {
-      return 'Nothing worked.';
-    }
-    
-    if (theOnes[last] !== array.length - 1) {
-      array[theOnes[last]] = 0;
-      array[theOnes[last] + 1] = 1;
-      runThroughCombos(n, array);
+  var indexIncrement = function(array) {
+    if (array[1] === n - 1) {
+      array[1] = 0;
+      array[0]++;
     } else {
-      array[theOnes[last]] = 0;
-      array[theOnes[0]] = 0;
-      var anchorPoint = theOnes[theOnes.length - 1];
-      for (var i = anchorPoint + 1; i <= anchorPoint + (theOnes.length / 2); i++) {
-      // set array[theOnes[theOnes.length - 1]] to 1, as well as 
-        array[i] = 1;
+      array[1]++;
+    }
+    return array;
+  };
+
+  // create function to run checks on a board setup, and make changes -> re-run if not passing
+  var checkBoard = function(board) {
+    // if the board passes Rooks checks
+    if (board.hasAnyRooksConflicts() === false) {
+      // make solution the current board
+      solution = board;
+      // and return
+      return;
+
+    } else { // otherwise...
+      // if the entire last row of the board is 1's (aka no more combinations left)
+      if (board.get((n - 1).toString()).includes(0) === false) {
+        // stop
+        return;
+      } else {
+        // otherwise
+        var anchor;  // will represent anchor point to make changes in positions (will be an index within 'positions')
+        if (positions[n - 1][0] === n - 1 && positions[n - 1][1] === n - 1) {  // when last elemnent in positions is the last possible index (n*n - 1)
+          // iterate backwards through positions checking if positions[i][1] is 1 less than positions[i + 1]
+          for (var i = n - 2; i >= 0; i--) {
+            if (positions[i][1] === positions[i + 1] - 1 && positions[i][0] === positions[i + 1][0]) { // if current colIndex is 1 less than next
+              board.togglePiece.apply(board, positions[i]);
+              continue;
+            // set anchor point at index in positions where the next lower index is not current index's value - 1
+            } else if (positions[i][1] === n - 1 && positions[i][0] === positions[i + 1][0] - 1) {
+              board.togglePiece.apply(board, positions[i]);
+              continue;
+            } else {
+              board.togglePiece.apply(board, positions[i]);
+              anchor = i;
+              break;
+            }
+          }
+          // increment positions at index anchor up 1 & toggle
+          indexIncrement(positions[anchor]);
+          board.togglePiece.apply(board, positions[anchor]);
+
+          // iterate forward from anchor, setting the value of positions[anchor] to positions[anchor - 1] + 1, etc...
+          for (var j = anchor + 1; j < positions.length; j++) {
+            var last = positions[j - 1].slice();
+            positions[j] = indexIncrement(last);
+            board.togglePiece.apply(board, positions[j]);
+          }
+
+          // set last space on board back to a 0
+          board.togglePiece.apply(board, [n - 1, n - 1]);
+          // and recursively re-run checkBoard
+          checkBoard(board);
+
+        } else { // if the rightmost 1 in the array is not the last element...
+          // make the current right-most 1 a 0...
+          board.togglePiece.apply(board, positions[n - 1]);
+          // make the NEXT element a 1 (moving to first index of next row if necessary)...
+          indexIncrement(positions[n - 1]);
+          board.togglePiece.apply(board, positions[n - 1]);
+          // and recursively re-run checkBoard on the new array
+          checkBoard(board);
+        }
       }
-      runThroughCombos(n, array);
     }
   };
 
-  var createStartingArray = function(n) {
+  checkBoard(testBoard);
+  
+
+
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  return solution;
+};
+
+// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+window.countNRooksSolutions = function(n) {
+  var solutionCount = 0;
+
+  var createStartingArray = function(n) {  // creates a single array of n*n values with the first n values being 1
     var result = [];
     for (var i = 1; i <= n; i++) {
       result.push(1);
     }
-    for (var j = 1; j <= n * 3; j++) {
+    for (var j = 1; j <= n * (n - 1); j++) {
       result.push(0);
     }
     return result;
   };
 
-  runThroughCombos(n, createStartingArray(n));
+  var matricize = function(n, array) {
+    var result = [];  // will become final matrix outer array
 
+    // iterate through passed in array (n * n number of elements), increment by n
+    for (var i = 0; i < array.length; i += n) {
+      // push an array of n elements to result
+      result.push(array.slice(i, i + n));
+    }
+    
+    //return newly formed matrix array result
+    return result;
+  };
 
+  // create starting test Board based on n (entire first row will be 1's)
+  var testBoard = new Board(matricize(n, createStartingArray(n)));
 
-  //console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  //return solution;
-};
+  // create set of starting indices based on n
+  var positions = function() {
+    var result = [];
+    for (var i = 0; i < n; i++) {
+      result.push([0, i]);
+    }
+    return result;
+  }();  // i.e. [[0, 0], [1, 3], [2, 0], [3, 3]] ==> starting indices for n = 4 ([row, column])
 
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var indexIncrement = function(array) {
+    if (array[1] === n - 1) {
+      array[1] = 0;
+      array[0]++;
+    } else {
+      array[1]++;
+    }
+    return array;
+  };
+
+  // create function to run checks on a board setup, and make changes -> re-run if not passing
+  var checkBoard = function(board) {
+    // if the board passes Rooks checks
+    if (board.hasAnyRooksConflicts() === false) {
+      // make solution the current board
+      solution = board;
+      // and return
+      return;
+
+    } else { // otherwise...
+      // if the entire last row of the board is 1's (aka no more combinations left)
+      if (board.get((n - 1).toString()).includes(0) === false) {
+        // stop
+        return;
+      } else {
+        // otherwise
+        var anchor;  // will represent anchor point to make changes in positions (will be an index within 'positions')
+        if (positions[n - 1][0] === n - 1 && positions[n - 1][1] === n - 1) {  // when last elemnent in positions is the last possible index (n*n - 1)
+          // iterate backwards through positions checking if positions[i][1] is 1 less than positions[i + 1]
+          for (var i = n - 2; i >= 0; i--) {
+            if (positions[i][1] === positions[i + 1] - 1 && positions[i][0] === positions[i + 1][0]) { // if current colIndex is 1 less than next
+              board.togglePiece.apply(board, positions[i]);
+              continue;
+            // set anchor point at index in positions where the next lower index is not current index's value - 1
+            } else if (positions[i][1] === n - 1 && positions[i][0] === positions[i + 1][0] - 1) {
+              board.togglePiece.apply(board, positions[i]);
+              continue;
+            } else {
+              board.togglePiece.apply(board, positions[i]);
+              anchor = i;
+              break;
+            }
+          }
+          // increment positions at index anchor up 1 & toggle
+          indexIncrement(positions[anchor]);
+          board.togglePiece.apply(board, positions[anchor]);
+
+          // iterate forward from anchor, setting the value of positions[anchor] to positions[anchor - 1] + 1, etc...
+          for (var j = anchor + 1; j < positions.length; j++) {
+            var last = positions[j - 1].slice();
+            positions[j] = indexIncrement(last);
+            board.togglePiece.apply(board, positions[j]);
+          }
+
+          // set last space on board back to a 0
+          board.togglePiece.apply(board, [n - 1, n - 1]);
+          // and recursively re-run checkBoard
+          checkBoard(board);
+
+        } else { // if the rightmost 1 in the array is not the last element...
+          // make the current right-most 1 a 0...
+          board.togglePiece.apply(board, positions[n - 1]);
+          // make the NEXT element a 1 (moving to first index of next row if necessary)...
+          indexIncrement(positions[n - 1]);
+          board.togglePiece.apply(board, positions[n - 1]);
+          // and recursively re-run checkBoard on the new array
+          checkBoard(board);
+        }
+      }
+    }
+  };
+
+  checkBoard(testBoard);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -123,19 +268,3 @@ window.countNQueensSolutions = function(n) {
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
-
-/* EXAMPLE MATRIX
-
-[[0, 1, 0, 0],
- [0, 0, 0, 1],
- [0, 0, 1, 0],
- [1, 0, 0, 0]]
-
-STEPS FOR ROOKS:
-
-1.  Come up with a combo
-2.  Run in through row conflict check
-3.  If it passes, return it OR add 1 to solutionCount (depending on which function)
-
-
-*/
